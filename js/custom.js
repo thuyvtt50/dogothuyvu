4bbe3a5148deee928acd0940ef80844fc3238e55
thuyvtt50 <142789690+thuyvtt50@users.noreply.github.com>
2024-03-27 14:37:19 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+   	 loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    autoplayTimeout:50000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+  })
+$('#danhmuc').owlCarousel({
+   	 loop:true,	// cho lap lại
+	items:4,	// xuất hiện 3 items thôi
+	margin:30,
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    autoplayTimeout:50000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+  })
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
